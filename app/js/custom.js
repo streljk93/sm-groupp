f5f565242edc17ce589badf1bb9155defc97afa8
jk <strel.jk.93@gmail.com>
2018-06-05 01:12:07 +0300

    fixed header

---
+++ app/js/custom.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// slick
+$('.jheader__background').slick({
+    arrows: false,
+    dots: false,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    infinite: true,
+    speed: 1000,
+    fade: true,
+})
