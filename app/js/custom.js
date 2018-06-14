142456d44f8bb3637bd484e0877aef0d1e8b0e1b
jk <strel.jk.93@gmail.com>
2018-06-14 18:34:03 +0300

    fixed main page & add footer

--- app/js/custom.js
+++ app/js/custom.js
@@ -12,5 +12,81 @@ $('.jheader__background').slick({
     fade: true,
 });
 
+$('.slick-article').slick({
+    dots: false,
+    speed: 300,
+    slidesToShow: 3,
+    adaptiveHeight: true,
+    mobileFirst: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                prevArrow: '<i class="angle double left icon" style="position:absolute;top:45%;left: -25px;color:#00B5AA;z-index:1;font-size:40px;cursor:pointer;"></i>',
+                nextArrow: '<i class="angle double right icon" style="position:absolute;top:45%;right: -30px;color:#00B5AA;z-index:1;font-size:40px;cursor:pointer;"></i>',
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                prevArrow: '<i class="angle double left icon" style="position:absolute;top:45%;left: -25px;color:#00B5AA;z-index:1;font-size:40px;cursor:pointer;"></i>',
+                nextArrow: '<i class="angle double right icon" style="position:absolute;top:45%;right: -29px;color:#00B5AA;z-index:1;font-size:40px;cursor:pointer;"></i>',
+            }
+        },
+        {
+            breakpoint: 0,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                centerMode: false,
+                prevArrow: '<i class="angle double left icon" style="position:absolute;top:45%;left: -10px;color:#00B5AB;z-index:1;font-size:40px;cursor:pointer;"></i>',
+                nextArrow: '<i class="angle double right icon" style="position:absolute;top:45%;right: -13px;color:#00B5AB;z-index:1;font-size:40px;cursor:pointer;"></i>',
+            }
+        }
+    ]
+});
+
+$('.slick-comment').slick({
+    dots: false,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    mobileFirst: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                prevArrow: '<i class="angle double left icon" style="position:absolute;top:45%;left: -40px;color:#00B5AA;z-index:1;font-size:40px;cursor:pointer;"></i>',
+                nextArrow: '<i class="angle double right icon" style="position:absolute;top:45%;right: -45px;color:#00B5AA;z-index:1;font-size:40px;cursor:pointer;"></i>',
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                prevArrow: '<i class="angle double left icon" style="position:absolute;top:45%;left: -40px;color:#00B5AA;z-index:1;font-size:40px;cursor:pointer;"></i>',
+                nextArrow: '<i class="angle double right icon" style="position:absolute;top:45%;right: -44px;color:#00B5AA;z-index:1;font-size:40px;cursor:pointer;"></i>',
+            }
+        },
+        {
+            breakpoint: 0,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                centerMode: false,
+                prevArrow: '<i class="angle double left icon" style="position:absolute;top:45%;left: -25px;color:#00B5AB;z-index:1;font-size:40px;cursor:pointer;"></i>',
+                nextArrow: '<i class="angle double right icon" style="position:absolute;top:45%;right: -28px;color:#00B5AB;z-index:1;font-size:40px;cursor:pointer;"></i>',
+            }
+        }
+    ]
+});
+
 $('.ui.sidebar').sidebar('attach events', '.item-open-close');
 $('.ui.sticky').sticky();
